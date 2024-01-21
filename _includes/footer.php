b3d1c590f526829c335ac2872a233b2bd9b3a142
Joseph Keane <itjosephk2@users.noreply.github.com>
2024-01-21 15:37:28 +0000

    initial commit

---
+++ _includes/footer.php
@@ -0,0 +1,36 @@
+<section id="contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 text-center">
+                <h2 class="section-heading">Let's Get In Touch!</h2>
+                <hr class="primary">
+                <p>Ready to get fit today? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+            </div>
+
+              <div class="col-lg-3 text-center">
+                  <i class="fa fa-phone fa-3x sr-contact"></i>
+                  <p>086-1727063</p>
+              </div>
+
+              <div class="col-lg-6 text-center">
+                  <ul class="list-inline banner-social-buttons">
+                    <li>
+                      <a href="https://twitter.com/elevatefp?lang=en" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                    </li>
+                    <li>
+                      <a href="https://www.instagram.com/elevate_fp/?hl=en" class="btn btn-default btn-lg"><i class="fa fa-instagram fa-fw"></i> <span class="network-name">Instagram</span></a>
+                    </li>
+                    <li>
+                      <a href="https://www.facebook.com/elevatefp/" class="btn btn-default btn-lg"><i class="fa fa-facebook fa-fw"></i> <span class="network-name">Facebook</span></a>
+                    </li>
+                  </ul>
+              </div>
+
+              <div class="col-lg-3 text-center">
+                  <i class="fa fa-envelope-o fa-3x sr-contact"></i>
+                  <p><a href="mailto:elevatefp@gmail.com" class="email" style="color: black; opacity: 1;">info@elevatefp.ie</a></p>
+              </div>
+
+        </div>
+    </div>
+</section>
---
+++ index.php
@@ -0,0 +1,375 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <?php include "_includes/head.php" ?>
+
+<body id="page-top">
+
+    <?php include "_includes/nav.php" ?>
+
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner">
+                <h1 id="homeHeading" class="text-center">Elevate</h1>
+                <hr class="home-page">
+                <p></p>
+                <a href="#about" class="btn btn-primary btn-xl page-scroll">Get fit now</a>
+            </div>
+        </div>
+    </header>
+
+    <section class="bg-primary" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">About The Gym</h2>
+                    <hr class="light">
+                    <p class="text-faded">Elevate Fitness &amp; Performance is a Strength &amp; conditioning gym that caters to all levels of fitness. We offer exceptional coaching in a fun friendly community. We have a class or program to suit everyone from Strength &amp; Conditioning to Bootcamp classes to Olympic lifting. Personal training also available. Strength & conditioning gym that caters to all levels of fitness. We offer exceptional coaching in a fun friendly community. We have a class or program to suit everyone from Strength & Conditioning to Bootcamp classes to Olympic lifting. Personal training also available. </p>
+                    <!-- <a href="#services" class="page-scroll btn btn-default btn-xl sr-button">Get Started!</a> -->
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="img-breaker">
+    </section>
+
+    <section id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Services</h2>
+                    <hr class="primary">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa-4x glyphicon glyphicon-scale text-primary sr-icons"></i>
+                        <h3>Boot Camp</h3>
+                        <p class="text-muted">A High intensity interval training class used to improve endurance, use energy better, boosts metabolism, helps you lose fat and not muscle, good for heart health &amp; its challenging. This is a pay as you go class. No monthly commitment. </p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-heart text-primary sr-icons"></i>
+                        <h3>S &amp; C</h3>
+                        <p class="text-muted">An hour long class that is used to help with injury prevention, get stronger, get fitter, improve plyometric’s, speed &amp; agility, endurance and core stability. </p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-child text-primary sr-icons"></i>
+                        <h3>Olympic Lifting</h3>
+                        <p class="text-muted">The two movements in Olympic weightlifting are the snatch and clean &amp; jerk. The benefits of Olympic lifting are that it increases your power, improves jumping ability, increases sprinting speed, improves body composition and improves flexibility. </p>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="container time-table">
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th>Time</th>
+                <th>Monday</th>
+                <th>Tuesday</th>
+                <th>Wednseday</th>
+                <th>Thursday</th>
+                <th>Friday</th>
+                <th>Saturday</th>
+                <th>Sunday</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>6.30am</th>
+                <td>Strength or Skill &amp; Conditioning</td>
+                <td>Strength or Skill &amp; Conditioning</td>
+                <td>Strength or Skill &amp; Conditioning</td>
+                <td>Strength or Skill &amp; Conditioning</td>
+                <td>Strength or Skill &amp; Conditioning</td>
+                <td>Closed</td>
+                <td>Closed</td>
+              </tr>
+              <tr>
+                <th>8am</th>
+                <td>Closed</td>
+                <td>Closed</td>
+                <td>Closed</td>
+                <td>Closed</td>
+                <td>Closed</td>
+                <td>Closed</td>
+                <td>Closed</td>
+              </tr>
+              <tr>
+                <th>9am</th>
+                <td>Open Gym</td>
+                <td>Open Gym</td>
+                <td>Open Gym</td>
+                <td>Open Gym</td>
+                <td>Open Gym</td>
+                <td>Open Gym &amp; Olympic Lifting Class</td>
+                <td>Closed</td>
+              </tr>
+              <tr>
+                <th>10am</th>
+                <td>Bootcamp €5</td>
+                <td>Basic Strength &amp; Conditioning €5</td>
+                <td>Bootcamp €5</td>
+                <td>Basic Strength &amp; Conditioning €5</td>
+                <td>Bootcamp €5</td>
+                <td>Conditioning €5</td>
+                <td>Closed</td>
+              </tr>
+              <tr>
+                <th>11am</th>
+                <td>Open Gym</td>
+                <td>Open Gym</td>
+                <td>Open Gym</td>
+                <td>Open Gym</td>
+                <td>Open Gym</td>
+                <td>Open Gym</td>
+                <td>Closed</td>
+              </tr>
+              <tr>
+                <th>12pm</th>
+                <td>Closed</td>
+                <td>Closed</td>
+                <td>Closed</td>
+                <td>Closed</td>
+                <td>Closed</td>
+                <td>Closed</td>
+                <td>Closed</td>
+              </tr>
+              <tr>
+                <th>4pm</th>
+                <td>Open Gym</td>
+                <td>Open Gym</td>
+                <td>Open Gym</td>
+                <td>Open Gym</td>
+                <td>Open Gym</td>
+                <td>Closed</td>
+                <td>Closed</td>
+              </tr>
+              <tr>
+                <th>5pm</th>
+                <td>Strength or Skill &amp; Conditioning</td>
+                <td>Strength or Skill &amp; Conditioning</td>
+                <td>Strength or Skill &amp; Conditioning</td>
+                <td>Strength or Skill &amp; Conditioning</td>
+                <td>Strength or Skill &amp; Conditioning</td>
+                <td>Closed</td>
+                <td>Closed</td>
+              </tr>
+              <tr>
+                <th>6pm</th>
+                <td>Strength or Skill &amp; Conditioning</td>
+                <td>Strength or Skill &amp; Conditioning</td>
+                <td>Strength or Skill &amp; Conditioning</td>
+                <td>Strength or Skill &amp; Conditioning</td>
+                <td>Strength or Skill &amp; Conditioning</td>
+                <td>Closed</td>
+                <td>Closed</td>
+              </tr>
+              <tr>
+                <th>7pm</th>
+                <td>Bootcamp €5</td>
+                <td>Basic Strength &amp; Conditioning €5</td>
+                <td>Bootcamp €5</td>
+                <td>Basic Strength &amp; Conditioning €5</td>
+                <td>Bootcamp €5</td>
+                <td>Closed</td>
+                <td>Closed</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+    </section>
+
+    <section class="no-padding" id="portfolio">
+        <div class="container-fluid">
+            <div class="row no-gutter popup-gallery">
+
+                <div class="col-lg-4 col-sm-6">
+                  <div  class="carousel slide" data-ride="carousel">
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner">
+
+                      <div class="item active">
+                        <img src="img/portfolio/1.jpg" alt="">
+                      </div>
+
+                      <div class="item">
+                        <img src="img/portfolio/2.jpg" alt="">
+                      </div>
+
+                      <div class="item">
+                        <img src="img/portfolio/3.jpg" alt="">
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                  <div class="carousel slide" data-ride="carousel">
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner">
+
+                      <div class="item active">
+                        <img src="img/portfolio/4.jpg" alt="">
+                      </div>
+
+                      <div class="item">
+                        <img src="img/portfolio/5.jpg" alt="">
+                      </div>
+
+                      <div class="item">
+                        <img src="img/portfolio/6.jpg" alt="">
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                  <div class="carousel slide" data-ride="carousel">
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner">
+
+                      <div class="item active">
+                        <img src="img/portfolio/7.jpg" alt="">
+                      </div>
+
+                      <div class="item">
+                        <img src="img/portfolio/8.jpg" alt="">
+                      </div>
+
+                      <div class="item">
+                        <img src="img/portfolio/9.jpg" alt="">
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                  <div id="myCarouselOne" class="carousel slide" data-ride="carousel">
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner">
+
+                      <div class="item active">
+                        <img src="img/portfolio/10.jpg" alt="">
+                      </div>
+
+                      <div class="item">
+                        <img src="img/portfolio/11.jpg" alt="">
+                      </div>
+
+                      <div class="item">
+                        <img src="img/portfolio/12.jpg" alt="">
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                  <div  class="carousel slide" data-ride="carousel">
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner">
+
+                      <div class="item active">
+                        <img src="img/portfolio/13.jpg" alt="">
+                      </div>
+
+                      <div class="item">
+                        <img src="img/portfolio/14.jpg" alt="">
+                      </div>
+
+                      <div class="item">
+                        <img src="img/portfolio/15.jpg" alt="">
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                  <div class="carousel slide" data-ride="carousel">
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner">
+
+                      <div class="item active">
+                        <img src="img/portfolio/16.jpg" alt="">
+                      </div>
+
+                      <div class="item">
+                        <img src="img/portfolio/17.jpg" alt="">
+                      </div>
+
+                      <div class="item">
+                        <img src="img/portfolio/18.jpg" alt="">
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+    <aside class="bg-dark">
+
+        <div class="container text-center">
+              <!-- Begin MailChimp Signup Form -->
+                <div id="mc_embed_signup">
+                  <form action="//elevatefp.us14.list-manage.com/subscribe/post?u=ed5e0381722c23426861e234a&amp;id=308ed1b40e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+
+                    <div class="form-group" id="mc_embed_signup_scroll">
+                      <h2>Subscribe to our mailing list</h2>
+                    <div class="indicates-required"></div>
+
+                    <div class="form-group" class="mc-field-group chimp-block">
+                      <label for="mce-EMAIL" class="chimp-label">Email Address
+                      </label>
+                      <input class="img-rounded" type="email" value="" name="EMAIL" class="chimp-box required email" id="mce-EMAIL">
+                    </div>
+
+                    <div class="form-group" class="mc-field-group chimp-block">
+                      <label for="mce-FNAME" class="chimp-label">First Name </label>
+                      <input class="img-rounded" type="text" value="" name="FNAME" class="chimp-box " id="mce-FNAME">
+                    </div>
+
+                    <div class="form-group" class="mc-field-group chimp-block">
+                      <label for="mce-LNAME" class="chimp-label">Last Name </label>
+                      <input class="img-rounded" type="text" value="" name="LNAME" class="chimp-box " id="mce-LNAME">
+                    </div>
+
+                    <div class="form-group" id="mce-responses" class="clear">
+                      <div class="response" id="mce-error-response" style="display:none"></div>
+                        <div class="response" id="mce-success-response" style="display:none">
+                        </div>
+                      </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+
+                      <div class="form-group" style="position: absolute; left: -5000px;" aria-hidden="true">
+                        <input class="img-rounded" type="text" name="b_ed5e0381722c23426861e234a_308ed1b40e" tabindex="-1" value="">
+                      </div>
+                      <div class="form-group" class="clear">
+                        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-primary btn-xl">
+                      </div>
+                    </div>
+                  </form>
+                </div>
+                <!--End mc_embed_signup-->
+        </div>
+    </aside>
+
+    <?php include "_includes/footer.php" ?>
+    <?php include "_includes/js.php" ?>
+
+</body>
+
+</html>
